4f2582f2f918600699e8ef02dc90a2972360a90f
Aldrin Dominique Natividad <aldmnatividad@gmail.com>
2020-12-15 01:24:57 -0500

    Clean up and description change

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, CssBaseline, Grid } from '@material-ui/core';
+import { CssBaseline, Grid } from '@material-ui/core';
 import Header from './Header';
 import Content from './Content';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
@@ -14,18 +14,18 @@ export default function App() {
   return (
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
-      <Grid container direction="column">
-        <Grid item>
-          <Header />
-        </Grid>
-        <Grid item container>
-          <Grid item xs={0} sm={2} /> {/* This is how you do paddings within a container! */}
-          <Grid item xs={12} sm={8}> {/* These break points should equal all the way up to 12 */}
-            <Content />
+        <Grid container direction="column">
+          <Grid item>
+            <Header />
           </Grid>
-          <Grid item xs={0} sm={2} /> {/* This is how you do paddings within a container! (part 2)*/}
+          <Grid item container>
+            <Grid item xs={0} sm={2} /> {/* This is how you do paddings within a container! */}
+            <Grid item xs={12} sm={8}> {/* These break points should equal all the way up to 12 */}
+              <Content />
+            </Grid>
+            <Grid item xs={0} sm={2} /> {/* This is how you do paddings within a container! (part 2)*/}
+          </Grid>
         </Grid>
-      </Grid>
       </ThemeProvider>
   );
 }
